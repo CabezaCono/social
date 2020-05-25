763179834fbb8435a30eff8692fd6916b0bc07c2
cabeza_cono <pos-12-ra@hotmail.es>
2020-05-25 14:43:28 +0200

    FIN VIDEO 28

---
+++ tests/Unit/Http/Resources/CommentResourceTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit\Http\Resources;
+
+use App\Models\Comment;
+use App\Models\Status;
+use Tests\TestCase;
+use App\Http\Resources\CommentResource;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CommentResourceTest extends TestCase
+{
+    use RefreshDatabase;
+    /** @test */
+
+    function a_comment_resources_must_have_the_necessary_fields()
+    {
+        $comment = factory(Comment::class)->create();
+
+        $commentresource = CommentResource::make($comment)->resolve();
+
+
+        $this->assertEquals($comment->body, $commentresource['body']);
+    }
+}
