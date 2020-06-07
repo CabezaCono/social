f2879e429af90ad7e63cfddfdafe5f8095c681b6
cabeza_cono <pos-12-ra@hotmail.es>
2020-06-07 12:43:05 +0200

    FIN VIDEO 60

--- tests/Unit/Models/FriendshipTest.php
+++ tests/Unit/Models/FriendshipTest.php
@@ -33,4 +33,30 @@ class FriendshipTest extends TestCase
 
         $this->assertInstanceOf(User::class, $friendship->recipient);
     }
+
+    /** @test */
+
+    function can_find_friendship_by_sender_and_recipient()
+    {
+        $sender = factory(User::class)->create();
+        $recipient = factory(User::class)->create();
+
+        factory(Friendship::class, 2)->create(['recipient_id' => $recipient->id]);
+        factory(Friendship::class, 2)->create(['sender_id' => $recipient->id]);
+
+        Friendship::create([
+            'sender_id' => $sender->id,
+            'recipient_id' => $recipient->id,
+        ]);
+
+        $foundFriendship = Friendship::betweenUsers($sender, $recipient)->first();
+
+        $this->assertEquals($sender->id, $foundFriendship->sender_id);
+        $this->assertEquals($recipient->id, $foundFriendship->recipient_id);
+
+        $foundFriendship2 = Friendship::betweenUsers($recipient, $sender)->first();
+
+        $this->assertEquals($sender->id, $foundFriendship2->sender_id);
+        $this->assertEquals($recipient->id, $foundFriendship2->recipient_id);
+    }
 }
