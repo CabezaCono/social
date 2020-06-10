0786a54897f754e34f9222044528f11d251adc23
cabeza_cono <pos-12-ra@hotmail.es>
2020-06-10 20:18:25 +0200

    FIN VIDEO 69

--- resources/js/components/StatusListItem.vue
+++ resources/js/components/StatusListItem.vue
@@ -23,32 +23,17 @@
                 <span dusk="likes-count">{{ status.likes_count }}</span>
             </div>
         </div>
-        <div class="card-footer">
+        <div class="card-footer pb-0" v-if="isAuthenticated || status.comments.length">
 
             <comment-list
                 :comments="status.comments"
                 :status-id="status.id"
             ></comment-list>
 
-            <form @submit.prevent="addComment" v-if="isAuthenticated">
-                <div class="d-flex align-items-center">
-                    <img class="rounded shadow-sm mr-2" width="34px"
-                         :src="currentUser.avatar"
-                         :alt="currentUser.name">
-                    <div class="input-group">
-                        <textarea v-model="newComment"
-                                  class="form-control border-0 shadow-sm"
-                                  name="comment"
-                                  placeholder="Escribe un comentario ..."
-                                  required
-                                  rows="1"
-                        ></textarea>
-                        <div class="input-group-append">
-                            <button class="btn btn-primary" dusk="comment-btn">Enviar</button>
-                        </div>
-                    </div>
-                </div>
-            </form>
+            <comment-form
+                :status-id="status.id"
+            ></comment-form>
+
         </div>
     </div>
 </template>
@@ -56,33 +41,15 @@
 <script>
     import LikeBtn from "./LikeBtn";
     import CommentList from "./CommentList";
+    import CommentForm from "./CommentForm";
     export default {
+        components: { LikeBtn, CommentList, CommentForm },
         props: {
             status: {
                 type: Object,
                 required: true
             }
         },
-
-        components: { LikeBtn, CommentList },
-
-        data() {
-            return {
-                newComment: '',
-            }
-        },
-        methods: {
-            addComment() {
-                axios.post(`/statuses/${this.status.id}/comments`, {body: this.newComment})
-                    .then(res => {
-                        this.newComment = '';
-                        EventBus.$emit(`statuses.${this.status.id}.comments`, res.data.data);
-                    })
-                    .catch(err => {
-                    console.log(err.response.data);
-                });
-            },
-        }
     }
 </script>
 
